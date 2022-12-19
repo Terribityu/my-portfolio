c2ac29e2080ee4c866eb6141323d144ddd252c92
John Rey Sto Domingo <stodomingojohnrey5@gmail.com>
2022-12-20 00:02:22 +0800

    Initial Commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -37,13 +37,13 @@ const Contact = () => {
             <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Terribityu</h5>
-            <a href="https://m.me/fascinatingact" target='_blank'>Send a message</a>
+            <a href="https://m.me/fascinatingact" rel="noreferrer" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone=1234567890" target='_blank'>Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=1234567890" rel="noreferrer" target='_blank'>Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTION */}
